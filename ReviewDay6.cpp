acd0609dfa139691185b16df284131022e08742d
tejal <tejal17trt@gmail.com>
2021-05-25 11:00:28 +0530

    till Day8

---
+++ ReviewDay6.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        for(int i=0;i<s.size();i=i+2)
+        {
+            cout<<s[i];
+        }
+        cout<<" ";
+        for(int i=1;i<s.size();i=i+2)
+            cout<<s[i];
+        cout<<endl;
+    }
+    return 0;
+}
+
